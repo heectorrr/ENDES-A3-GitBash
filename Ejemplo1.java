e09b393e87493ac9e19a7067743b04f0c8c0d2c5
Hector <hecotron2102@gmail.com>
2024-03-18 10:44:43 +0100

    Mi primer commit

---
+++ Ejemplo1.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Ejemplo1 {
+	/* programa holamundo*/
+	public static void main (String[]args) {
+		//Lo unico que hace este programa es mostrar un hola mundo por la pantalla
+		System.out.println("Hola");
+		System.out.print("Mundo");
+	}
+}
